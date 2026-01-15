f5b792cf6fdc01f3874753249e73f3d477450440
Mustafa21102005 <mustafa21102005@gmail.com>
2026-01-15 20:31:20 +0300

    chore: create note and view it

--- src/main.js
+++ src/main.js
@@ -2,16 +2,38 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
+import Nora from '@primevue/themes/nora'
 import 'primeicons/primeicons.css'
+import { definePreset } from '@primevue/themes'
+import ToastService from 'primevue/toastservice'
 
 const app = createApp(App)
+
+app.use(ToastService);
+
+const MyPreset = definePreset(Nora, {
+    semantic: {
+        primary: {
+            50:  '#eef2ff',
+            100: '#e0e7ff',
+            200: '#c7d2fe',
+            300: '#a5b4fc',
+            400: '#818cf8',
+            500: '#6366f1', // main primary
+            600: '#4f46e5',
+            700: '#4338ca',
+            800: '#3730a3',
+            900: '#312e81',
+            950: '#1e1b4b'
+        }
+    }
+})
 
 app.use(router)
 
 app.use(PrimeVue, {
     theme: {
-        preset: Aura
+        preset: MyPreset
     }
 })
 
